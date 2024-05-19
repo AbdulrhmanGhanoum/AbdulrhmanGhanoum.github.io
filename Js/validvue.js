ef381950a55524575c2d8bb43453080ff3b20af4
ABDULRHMAN GHANOUM <abdulrhman.ghanoum@ogr.sakarya.edu.tr>
2024-05-19 23:41:10 +0300

    for vue

--- Js/validvue.js
+++ Js/validvue.js
@@ -1,47 +1,34 @@
 const { createApp } = Vue;
 
-    createApp({
-        data() {
-            return {
-                name: '',
-                email: '',
-                errors: []
-            };
-        },
-        methods: {
-            checkForm() {
-                this.errors = [];
+createApp({
+    data() {
+        return {
+            name: '',
+            email: '',
+            errors: []
+        };
+    },
+    methods: {
+        checkForm(e) {
+            this.errors = [];
 
-                if (!this.name) {
-                    this.errors.push('Name required.');
-                }
-                if (!this.email) {
-                    this.errors.push('Email required.');
-                } else if (!this.validEmail(this.email)) {
-                    this.errors.push('Valid email required.');
-                }
-
-                return !this.errors.length;
-            },
-            validEmail(email) {
-                const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                return re.test(email);
-            },
-            handleSubmit(e) {
-                if (this.checkForm()) {
-                    // Form is valid, you can submit it or perform any action you need.
-                    alert('Form is valid');
-                } else {
-                    e.preventDefault();
-                }
-            },
-            handleButtonClick() {
-                if (this.checkForm()) {
-                    // Form is valid, you can submit it or perform any action you need.
-                    alert('Form is valid');
-                } else {
-                    alert('Form is invalid');
-                }
+            if (!this.name) {
+                this.errors.push('enter name v.');
             }
+            if (!this.email) {
+                this.errors.push('enter email.');
+            } else if (!this.validEmail(this.email)) {
+                this.errors.push('Enter a correct email.');
+            }
+            if (!this.errors.length) {
+                return true;
+            }
+
+            e.preventDefault();
+        },
+        validEmail(email) {
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
         }
-    }).mount('#app');
+    }
+}).mount('#app');
